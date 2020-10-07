f4210272de509bdecf06bd70f79a818f8aae2499
Spenser <spenser.davies@gmail.com>
2020-10-07 10:58:34 -0600

    react chat with firebase

--- App.js
+++ App.js
@@ -1,21 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// @refresh reset
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect, useCallback } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
+import { GiftedChat } from "react-native-gifted-chat";
+import {
+  StyleSheet,
+  Text,
+  View,
+  YellowBox,
+  TextInput,
+  Button,
+} from "react-native";
+import * as firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpkIKuIS_1HRCyaYsOzymJc8DGrcKJYYo",
+  authDomain: "chat-app-c8367.firebaseapp.com",
+  databaseURL: "https://chat-app-c8367.firebaseio.com",
+  projectId: "chat-app-c8367",
+  storageBucket: "chat-app-c8367.appspot.com",
+  messagingSenderId: "54245406412",
+  appId: "1:54245406412:web:a39c4670ff9e5966353469",
+  measurementId: "G-X7QZS1QB84",
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// YellowBox.ignoreWarnings(["Setting a timer for a long period of time"]);
+
+const db = firebase.firestore();
+
+const messagesRef = db.collection("messages");
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [user, setUser] = useState(null);
+  const [name, setName] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    readUser();
+    const unsubscribe = messagesRef.onSnapshot((querySnapshot) => {
+      const messagesFirestore = querySnapshot
+        .docChanges()
+        .filter(({ type }) => type === "added")
+        .map(({ doc }) => {
+          const message = doc.data();
+          return { ...message, createdAt: message.createdAt.toDate() };
+        })
+        .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
+      appendMessages(messagesFirestore);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const appendMessages = useCallback(
+    (messages) => {
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, messages)
+      );
+    },
+    [messages]
   );
+
+  async function readUser() {
+    const user = await AsyncStorage.getItem("user");
+    if (user) {
+      setUser(JSON.parse(user));
+    }
+  }
+  async function handlePress() {
+    const _id = Math.random().toString(36).substring(7);
+    const user = { _id, name };
+    await AsyncStorage.setItem("user", JSON.stringify(user));
+    setUser(user);
+  }
+  async function handleSend(messages) {
+    const writes = messages.map((m) => messagesRef.add(m));
+    await Promise.all(writes);
+  }
+
+  if (!user) {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Your Name"
+          value={name}
+          onChangeText={setName}
+        />
+        <Button title="Enter Chat" onPress={handlePress} />
+      </View>
+    );
+  }
+
+  return <GiftedChat messages={messages} user={user} onSend={handleSend} />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 30,
+  },
+  input: {
+    height: 50,
+    width: "100%",
+    borderWidth: 1,
+    padding: 15,
+    borderColor: "grey",
+    marginBottom: 20,
   },
 });
